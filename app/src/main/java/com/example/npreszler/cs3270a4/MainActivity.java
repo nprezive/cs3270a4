fd1afaad119d526371caa7893b876b5ed76b3219
Noah Preszler <npreszler@gmail.com>
2018-02-03 17:08:08 -0700

    Created mockup of app. No functionality.

--- app/src/main/java/com/example/npreszler/cs3270a4/MainActivity.java
+++ app/src/main/java/com/example/npreszler/cs3270a4/MainActivity.java
@@ -1,13 +1,23 @@
 package com.example.npreszler.cs3270a4;
 
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private FragmentManager fm;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        fm = getSupportFragmentManager();
+        fm.beginTransaction()
+                .replace(R.id.frame_totals, new FragmentTotals(), "fragTotals")
+                .replace(R.id.frame_tax, new FragmentTax(), "fragTax")
+                .replace(R.id.frame_items, new FragmentItems(), "fragItems")
+                .commit();
     }
 }
